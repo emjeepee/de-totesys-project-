524f0980f5da17055af438b851010e27a1ce47e3
Duncan <duncan.cornish.1999@gmail.com>
2025-05-28 14:45:01 +0100

    fix tests folder name and add test

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 def read_table(table_name, conn, after_time):
-    '''
+    """
     selects all the data from the specified table after the last run time
 
     takes the arguments for:
@@ -8,10 +8,17 @@ def read_table(table_name, conn, after_time):
     the time which to select records updated after (ie the last time the function was run)
 
     returns a string to be converted into json
-    '''
-    result = conn.run(f'''
-    SELECT * FROM :table_name
-    WHERE last_updated > :after_time
-    ''', table_name = table_name, after_time = after_time)
+    """
+    # try:
+    result = conn.run(
+        f"""
+        SELECT * FROM :table_name
+        WHERE last_updated > :after_time
+        """,
+        table_name=table_name,
+        after_time=after_time,
+    )
 
     return result
+    # except Exception:
+    #     raise ValueError
