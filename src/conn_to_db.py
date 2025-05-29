83b5eefe9f1786a2f8921344bdca3434e9faf5d2
Neil Hallard <nhallard1@gmail.com>
2025-05-29 14:17:22 +0100

    minor readability cleanup & fixing incorrect folder name

--- src/conn_to_db.py
+++ src/conn_to_db.py
@@ -1,7 +1,9 @@
 import os
 from pg8000.native import Connection
-#from pg8000.dbapi import Connection as RemoteConnection
+
+# from pg8000.dbapi import Connection as RemoteConnection
 from dotenv import load_dotenv
+
 
 def conn_to_db(DB_NAME="TESTDB"):
     load_dotenv()
@@ -9,7 +11,16 @@ def conn_to_db(DB_NAME="TESTDB"):
     password = os.environ[f"PG_{DB_NAME}_PASSWORD"]
     database = os.environ[f"PG_{DB_NAME}_DATABASE"]
     host = os.environ[f"PG_{DB_NAME}_HOST"]
-    port = os.environ[f"PG_{DB_NAME}_PORT"]#default: 5432?
-    return Connection(username,database=database,password=password,host=host,port=port,ssl_context=True)
-def close_db(conn:Connection):
+    port = os.environ[f"PG_{DB_NAME}_PORT"]  # default: 5432?
+    return Connection(
+        username,
+        database=database,
+        password=password,
+        host=host,
+        port=port,
+        ssl_context=True,
+    )
+
+
+def close_db(conn: Connection):
     conn.close()
