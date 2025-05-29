250c380bac67f8548f3867fdd44f74091abbd98b
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-05-29 09:59:03 +0100

    feat: add creation of Terraform code to provision the EventBridge that will react to changes to the ingestion bucket

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,12 +1,15 @@
-resource "aws_cloudwatch_event_bus" "trigger_second_lambda"{
-    name = "cloud_trigger_second_lambda"
+# Create a custom event bus
+resource "aws_cloudwatch_event_bus" "bus_for_events_from_ingestion_S3_bucket"{
+    name = "bus_for_events_from_ingestion_S3_bucket"
 }
 
 
-resource "aws_cloudwatch_event_rule" "trigger_on_s3_bucket_change" {
-  name        = "capture-ingestion-s3-bucket-change"
-  description = "Capture changes in s3 ingestion bucket files"
-  event_bus_name = aws_cloudwatch_event_bus.trigger_second_lambda
+# Create an event rule (to filter for events put on the event bus above
+# by the ingestion S3 bucket)
+resource "aws_cloudwatch_event_rule" "filter_for_events_from_ingestion_bucket" {
+  name        = "filter_for_events_from_ingestion_bucket"
+  description = "Filter for events put on the EventBridge bus by the ingestion S3 bucket when files are modified in the bucket or added to the bucket"
+  event_bus_name = aws_cloudwatch_event_bus.bus_for_events_from_ingestion_S3_bucket.name
 
   event_pattern = jsonencode({
     source: ["aws.s3"],
@@ -20,14 +23,18 @@ resource "aws_cloudwatch_event_rule" "trigger_on_s3_bucket_change" {
 }
 
 
-
-resource "aws_cloudwatch_event_target" "lambda_target" {
-  rule      = aws_cloudwatch_event_rule.trigger_on_s3_bucket_change.name
+# Set the target (the second lambda) for the rule created above
+resource "aws_cloudwatch_event_target" "target_second_lambda" {
+  rule      = aws_cloudwatch_event_rule.filter_for_events_from_ingestion_bucket.name
   arn       = XXX-lambda-function-XXX.arn
   target_id = "lambda-target"
 }
 
 
+#  Allow the ingestion S3 bucket to make notifications
+# that will go onto the custom bus created above
+# when files in the ingestion bucket are added to it or
+# when files already in it are modified
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.ingestion-bucket.bucket.id
   
@@ -37,14 +44,5 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   }
 }
 
-
-
-resource "aws_lambda_permission" "allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.xxx-second-lambda-function-xxx
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.invoke_second_lambda.arn
-                                                     }
 
 
