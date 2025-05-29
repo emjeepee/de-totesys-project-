9ff558ce1b8e44cbf037fa6b2fe778e3acd95e02
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-05-29 23:59:41 +0100

    feat: add more code to function write_to_ingestion_bucket(), which is not yet complete.

--- src/utils_write_to_ingestion_bucket.py
+++ src/utils_write_to_ingestion_bucket.py
@@ -1,32 +1,174 @@
-"""
-Sudo code:
-Create a function to:
-    take a given json data
-    write it to the S3 ingestion bucket
-
-"""
-
+from datetime import datetime
 import boto3
 import json
+
+
 # /src/utils.py
 def write_to_ingestion_bucket(data, bucket, file_location):
     """
-    This fucntion: 
-    1. Get the appropriate object from the ingestion bucket
-    2. Change the appropriate rows in the table respresented by the object
-    3. Put the object in the ingestion bucket, naming it with a timestamp in its name
-
+    This function:
+    1. Searches the ingestion bucket for all jsonified table lists
+            that are stored under a key that begins with 
+            file_location and gets the most recent list
+    2. Converts the most recent list to a python list of dictionaries           
+    3. Updates the appropriate rows in the python list
+    4. Creates a new json list to represent the updated table
+    5. Creates a timestamp string that will be part of the name of 
+            the key under which to store the jsonified list.
+    5. Stores in the ingestion bucket the new json list that represents 
+            the updated table. The key for this new json list looks like 
+            design/2025-05-28_15-45-03.json, where 'design' is the value 
+            of file_location and '2025-05-28_15-45-03' is a timestamp.  
 
     args:
-        data: a json string to be added to object_key
-        bucket_name: name of the target S3 bucket
-        object_key: file name to be saved as in the ingestion bucket.
+        data: a json list of dictionaries, each dictionary representing 
+            an updated row of a table in the ToteSys database.
+        bucket_name: a string that is the name of the ingestion S3 bucket.
+        file_location: a string that is the first part of the key under 
+            which this function will store in the ingestion bucket the 
+            json list that represents an updated table. Example: 'design'.
+
     returns:
-        none ? name of the key it saved in the bucket 
+        A python dictonary with these keys: 
+            content -- the value is ??????
 
     """
+
+    # 1) Get the most recent object that holds a json list that 
+    # represents a table: 
     client = boto3.client("s3")
-    file = client.get_object(Bucket=bucket, Key=file_location)
-    content = file["Body"].read().decode("utf-8")
-    file1 = client.get_object(Bucket=bucket, Key=file_location)
-    return {"content": content}
+    latest_table = get_most_recent_table_data(file_location, client, bucket)
+        # python list like [{}, {}, {}, etc]
+
+    # 2) Create a list of python dictionaries to represent the updated rows of data:
+    updated_rows = json.loads(data) # python list like [{}, {}, {}, etc]
+
+    # 3) Insert those updated rows into the retrieved json list of 
+    # dictionaries that represents the table:
+
+    # 4) Create a new timestamp and format it:
+
+    # 5) Make a string for the key under which to store the json list
+    # that represents the updated table. Include the formatted 
+    # timestamp as part of the key:
+
+    # 6) Store the json list that represents the updated table into
+    # in the ingestion bucket under the newly created key:
+
+
+    return 
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_most_recent_table_data(file_location: str, 
+                               S3_client,
+                               bucket_name: str
+                               ):
+    """
+    This function:
+        1) gets a list of every jsonified list of 
+                dictionaries in the bucket if their 
+                keys begin with file_location. The
+                list will be a python list 
+        2) sorts the list according to time, 
+                descending
+        3) gets the most recent list 
+        
+    Returns:
+        The most recent python list                
+
+    Args:
+        file_location: a string that looks like
+        'design'
+        S3_client: the boto3 S3 bucket client
+    bucket_name: a string for the bucket name      
+    """
+    response = S3_client.list_objects_v2(Bucket=bucket_name, Prefix=file_location)
+                # {
+                    # 'ResponseMetadata': { ...},
+                    #     ...
+                    # 'Contents': [
+                    #     { 
+                    #     'Key': 'design/xxx.json',
+                    #     [{...}, {...}, {...}, etc]
+                    #     },
+                    #     {
+                    #     'Key': 'design/yyy.json', 
+                    #     ...
+                    #     }
+                    #             ]
+                # }
+    keys_list = [dict['Key'] for dict in response.get('Contents', [])]
+                # ['design/2025-06-02_22-17-19-2513.json', 'design/2025-05-29_22-17-19-2513.json', etc]
+    latest_table_key = sorted(keys_list)[-1] # 'design/2025-06-02_22-17-19-2513.json'
+    response = S3_client.get_object(Bucket=bucket_name , Key=latest_table_key)
+    data = response['Body'].read().decode("utf-8")
+    data_as_py_list = json.loads(data)
+    return data_as_py_list
+                
+
+
+
+    
+def create_formatted_timestamp(timestamp):
+    """
+    This function:
+        1) creates a timestamp string of the format
+            'YYYY-MM-DD_HH-MM-SS'
+    Returns:
+        The formatted timestamp string
+    Args:
+        A timestamp string created by datetime.now(),
+        for example: '2025-05-29 22:17:19.251352'
+    """
+    formatted_ts = timestamp.strftime("%Y-%m-%d_%H-%M-%S")
+    return formatted_ts
+
+
+
+
+
+
+
+
+
+
+
+def update_rows_in_table(rows_list, table_list):
+    """
+    This function:
+        1) updates the appropriate rows in a 
+                python list that represesnts a table
+    Returns:
+        a python list that represents the updated table
+    Args:
+        rows_list: a python list of dictionaries, each 
+        dictionary representing a row that contains
+        updated data.
+        table_list: a python list of dictionaries that
+        represents a table                
+    """
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/test_write_to_ingestion.py
+++ tests/test_write_to_ingestion.py
@@ -5,23 +5,11 @@ import json
 from unittest.mock import Mock, patch
 from datetime import datetime
 import os
-from src.utils_write_to_ingestion_bucket import write_to_ingestion_bucket
-
-"""
-    This fucntion: 
-    1. Get the appropriate object from the ingestion bucket
-    2. Change the appropriate rows in the table respresented by the object
-    3. Put the object in the ingestion bucket, naming it with a timestamp in its name
+from src.utils_write_to_ingestion_bucket import write_to_ingestion_bucket, get_most_recent_table_data
 
 
-    args:
-        data: a json string to be added to object_key
-        bucket_name: name of the target S3 bucket
-        object_key: file name to be saved as in the ingestion bucket.
-    returns:
-        none ? name of the key it saved in the bucket 
 
-    """
+
 
 @pytest.fixture(scope="function")
 def aws_credentials():
@@ -32,55 +20,99 @@ def aws_credentials():
     os.environ["AWS_SESSION_TOKEN"] = "test"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
+
 @pytest.fixture(scope="module")
 def mock_S3_client():
     with mock_aws():
-        yield boto3.client('s3',region_name="eu-west-2")
-
-# Create mock data
-timestamp = datetime.now()
-bucket_name = "11-ingestion-bucket"
-object_key = f"ingested/design-{timestamp}.json"
-
-# Create mock bucket
-
-# Create object with mock data
-body = '{"name": "abdul", "team": 11, "project": "terraform"}'
+        yield boto3.client("s3", region_name="eu-west-2")
 
 
-# mock data 
-body_1 = '{"name": "Neil", "team": 12, "project": "writing JSON"}'
-mock_table = {"design": [{"name": "abdul", "team": 11, "project": "terraform"},
-                             {"name": "Mukund", "team": 11, "project": "terraform"},
-                             {"name": "Neil", "team": 11, "project": "terraform"}]
-                             }
-expected_table = [
-        {"name": "abdul", "team": 11, "project": "terraform"},
-        {"name": "Mukund", "team": 11, "project": "terraform"},
-        {"name": "Neil", "team": 12, "project": "writing JSON"}
-        ]
-                             
-mock_json = json.load(mock_table)
 
+def test_get_most_recent_table_data_returns_correct_list(mock_S3_client):
+    # arrange:
+    bucket_name = "11-ingestion-bucket"
 
-def test_it_can_read_object_from_ingestion_bucket(mock_S3_client):
+    # Create mock bucket
     mock_S3_client.create_bucket(
-        Bucket=bucket_name,
-    CreateBucketConfiguration={"LocationConstraint": "eu-west-2"})
+            Bucket=bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+                                )
+
+    # # Create object with mock data
+    # body = '{"name": "abdul", "team": 11, "project": "terraform"}'
+
+    # # mock data
+    # body_1 = '{"name": "Neil", "team": 12, "project": "writing JSON"}'
+
+    # create mock python tables
+    mock_table_1 =  [
+            {"name": "abdul", "team": 11, "project": "terraform"},
+            {"name": "Mukund", "team": 12, "project": "terraform"},
+            {"name": "Neil", "team": 13, "project": "terraform"},
+                    ]
+
+    mock_table_2 =  [
+            {"name": "abdul", "team": 14, "project": "terraform"},
+            {"name": "Mukund", "team": 15, "project": "terraform"},
+            {"name": "Neil", "team": 16, "project": "terraform"},
+                    ]
+
+    mock_table_3 =  [
+            {"name": "abdul", "team": 17, "project": "terraform"},
+            {"name": "Mukund", "team": 18, "project": "terraform"},
+            {"name": "Neil", "team": 19, "project": "terraform"},
+                    ]
+
+    # create mock jsonified tables:
+    mt_1_json = json.dumps(mock_table_1)
+    mt_2_json = json.dumps(mock_table_2)
+    mt_3_json = json.dumps(mock_table_3)
+
+    # create keys for mock jsonified tables:
+    key_1 = 'design/2025-05-29_22-17-19-251352'
+    key_2 = 'design/2025-05-29_22-07-19-251352'
+    key_3 = 'design/2025-05-29_21-57-19-251352'
+
+    # put the mock jsonified tables in the bucket:
+    mock_S3_client.put_object(Bucket=bucket_name , Key= key_1, Body=mt_1_json)
+    mock_S3_client.put_object(Bucket=bucket_name , Key= key_2, Body=mt_2_json)
+    mock_S3_client.put_object(Bucket=bucket_name , Key= key_3, Body=mt_3_json)
+    
+    expected_table = mock_table_1
+
+    # act:
+    result = get_most_recent_table_data('design', mock_S3_client, bucket_name)
+
+    # assert:
+    assert expected_table == result 
+
+
+
+
+
+
+@pytest.mark.skip
+def test_it_can_read_object_from_ingestion_bucket(mock_S3_client):
+
     mock_S3_client.put_object(Bucket=bucket_name, Key=object_key, Body=body)
 
     mock_data = {"name": "Mukund", "Team": 11, "project": "terraform"}
-    assert write_to_ingestion_bucket(json.dump(mock_data), bucket_name, object_key) == {"name": "Mukund", "Team": 11, "project": "terraform"}
+    assert write_to_ingestion_bucket(json.dump(mock_data), bucket_name, object_key) == {
+        "name": "Mukund",
+        "Team": 11,
+        "project": "terraform",
+    }
 
 
-
+@pytest.mark.skip
 def test_that_function_modifies_the_data_read_from_s3_bucket(mock_S3_client):
     # timestamp = datetime.now()
-    mock_S3_client.put_object(Bucket=bucket_name, Key=f"Test-key-{timestamp}", Body=mock_json)
-    assert write_to_ingestion_bucket(
-            json.loads(mock_data),
-            bucket_name,
-            object_key)["content1"] == expected_table
+    mock_S3_client.put_object(
+        Bucket=bucket_name, Key=f"Test-key-{timestamp}", Body=mock_json
+    )
+    assert write_to_ingestion_bucket(json.loads(body_1), bucket_name, object_key)["content1"] == expected_table
+    
+
 
 """
 
