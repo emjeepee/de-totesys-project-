dd620b0b9a66016f96aa927c2b928229edfe6751
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-05-29 10:21:38 +0100

    feat: add permission for second lambda to receive events from the Eventbridge that reacts to changes in the ingestion S3 bucket

--- terraform/IAM.tf
+++ terraform/IAM.tf
@@ -1,41 +1,14 @@
 
-# 
-data "aws_iam_policy_document" "allow-lambda-s3-put-access"{
-    statement {
-      actions = ["s3:PutObject"]
-       resources = ["arn:aws:s3:::${aws_s3_bucket.ingestion-bucket.bucket}"]
-    }
-}
-resource "aws_iam_policy" "first_lambda_policy"{
-    name = "s3_put_object_access"
-    policy = data.aws_iam_policy_document.allow-lambda-s3-put-access.json
-}
 
 
-resource "aws_iam_role" "s3PutObjectAccess" {
-    name = "give_access_to_write_to_s3_bucket"
-    assume_role_policy = data.aws_iam_policy_document.allow-lambda-s3-put-access.json
-  
-}
 
-resource "aws_iam_policy_attachment" "attach_policy" {
-    name = "attach-policy"
-    policy_arn = aws_iam_policy.first_lambda_policy.arn
-    roles = aws_iam_role.s3PutObjectAccess
-  
-}
-
-
-# Create role for second lambda to allow it to be triggered by EventBridge
-
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda-exec-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Principal = { Service = "lambda.amazonaws.com" },
-      Effect = "Allow",
-    }]
-  })
-}
+# Create a permission for the second lambda so that it can
+# receive events from the EventBridge to allow the invocation
+# of the second lambda
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.xxx-second-lambda-function-xxx
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.invoke_second_lambda.arn
+                                                     }
