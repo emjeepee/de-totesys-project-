cb8139d0af2a21c4f747027a2d81df3ba2d563f0
Amar <amar.walji@gmail.com>
2025-05-29 13:57:37 +0100

    deleted duplicate code

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -51,59 +51,6 @@ resource "aws_iam_role_policy_attachment" "first_lambda_s3_write_policy_attachme
   policy_arn = aws_iam_policy.s3_write_policy.arn
 }
 
-# ----------------
-#Lambda IAM role |
-# ---------------
-
-data "aws_iam_policy_document" "trust_policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "first_lambda_function_role" {
-    name = "role-${var.first_lambda_function}"
-    assume_role_policy = data.aws_iam_policy_document.trust_policy.json
-}
-
-#---------------------------------
-# Lambda IAM Policy for s3 Write |
-#--------------------------------
-
-data "aws_iam_policy_document" "s3_ingestion_data_policy_doc"{
-  statement {
-    actions = ["s3:Get*", "s3:List*"]
-    resources = [
-        "arn:aws:s3:::${var.ingestion-bucket}",
-        "arn:aws:s3:::${var.ingestion-bucket}/*"
-    ]
-  }
-  statement {
-    actions = ["s3:Put*"]
-    resources = [
-        "arn:aws:s3:::${var.ingestion-bucket}",
-        "arn:aws:s3:::${var.ingestion-bucket}/*"
-    ]
-  }
-}
-
-resource "aws_iam_policy" "s3_write_policy" {
-  name = "s3-policy-${var.first_lambda_function}-write"
-  policy = data.aws_iam_policy_document.s3_ingestion_data_policy_doc.json
-}
-
-resource "aws_iam_role_policy_attachment" "first_lambda_s3_write_policy_attachment" {
-  role = aws_iam_role.first_lambda_function_role.name
-  policy_arn = aws_iam_policy.s3_write_policy.arn
-}
-
 # ----------------------------------
 # Lambda IAM Policy for Cloudwatch |
 # ---------------------------------
