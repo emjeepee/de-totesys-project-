fd49742a29b50bb4828b319d8e44ca3719c59b5f
Neil Hallard <nhallard1@gmail.com>
2025-06-02 09:45:02 +0100

    fixed a missing paramenter

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import json
 from pg8000.native import Connection
 
-def read_table(table_name:str, conn:Connection, after_time):
+def read_table(table_name:str, conn:Connection, after_time:str):
     """
     selects all the data from the specified table after the last run time
 
