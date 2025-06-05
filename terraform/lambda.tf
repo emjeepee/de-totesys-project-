d8f30717c0931e8d76180e48d3f446dadf750d4a
Duncan <duncan.cornish.1999@gmail.com>
2025-06-05 09:48:08 +0100

    change lambda_archive to point to the right file but better

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,7 +9,7 @@ data "archive_file" "first_lambda_archive" {
   output_path      = "${path.module}/../function.zip" # place to store the zip before uploading to s3
 }
 
-data "archive_file" "first_layer_archive" {
+data "archive_file" "layer_archive" {
   type             = "zip"
   output_file_mode = "0666"
   source_dir       = "${path.module}/../layer/" # change this to the layer
@@ -25,14 +25,14 @@ resource "aws_s3_object" "first_lambda_deployment" {
 resource "aws_s3_object" "first_layer_deployment" {
   bucket = aws_s3_bucket.lambda-bucket.bucket
   key    = "first_lambda/layer.zip"
-  source = data.archive_file.first_layer_archive.output_path
+  source = data.archive_file.layer_archive.output_path
 }
 
 resource "aws_lambda_layer_version" "requests_layer" {
   layer_name          = "requests_layer"
   compatible_runtimes = [var.python_runtime]
   s3_bucket           = aws_s3_bucket.lambda-bucket.bucket
-  s3_key              = aws_s3_object.first_layer_deployment.key
+  s3_key              = aws_s3_object.layer_deployment.key
 }
 
 resource "aws_lambda_function" "extract_handler" {
@@ -43,7 +43,7 @@ resource "aws_lambda_function" "extract_handler" {
 
   function_name    = var.first_lambda_function
   role             = aws_iam_role.first_lambda_function_role.arn
-  handler          = "main.lambda_handler" # change this to point to the handler
+  handler          = "lambda_handler.lambda_handler" # change this to point to the handler
   runtime          = "python3.13"
   source_code_hash = data.archive_file.first_lambda_archive.output_base64sha256
 
