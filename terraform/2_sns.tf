1ad3ddfcee992ec8d53ef342490b02a0cd575a3b
Amar <amar.walji@gmail.com>
2025-06-06 09:59:33 +0100

    changed 2_..tf files to isolate problem on lambda.tf

--- terraform/2_sns.tf
+++ terraform/2_sns.tf
@@ -1,24 +1,24 @@
 
-#####################################
-#
-# Cloudwatch Logs for second lambda
-#
-#####################################
+# #####################################
+# #
+# # Cloudwatch Logs for second lambda
+# #
+# #####################################
 
 
-resource "aws_cloudwatch_metric_alarm" "SecondLambdaError"{
-    alarm_name = "SecondLambdaAlert"
-    comparison_operator = "GreaterThanOrEqualToThreshold"
-    evaluation_periods = "1"
-    metric_name = "Errors"
-    namespace = "AWS/Lambda"
-    statistic = "Sum"
-    threshold = 1
-    period = "60"
-    alarm_description = "Alert If second Lambda function errors"
-    alarm_actions = [aws_sns_topic.lambda_alerts.arn]
-    dimensions = {
-      FunctionName = var.second_lambda_function
-    }
+# resource "aws_cloudwatch_metric_alarm" "SecondLambdaError"{
+#     alarm_name = "SecondLambdaAlert"
+#     comparison_operator = "GreaterThanOrEqualToThreshold"
+#     evaluation_periods = "1"
+#     metric_name = "Errors"
+#     namespace = "AWS/Lambda"
+#     statistic = "Sum"
+#     threshold = 1
+#     period = "60"
+#     alarm_description = "Alert If second Lambda function errors"
+#     alarm_actions = [aws_sns_topic.lambda_alerts.arn]
+#     dimensions = {
+#       FunctionName = var.second_lambda_function
+#     }
 
-}
+# }
