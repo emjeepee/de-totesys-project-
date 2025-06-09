afe6b7e9b1ce291624308f5ebeace327c4b1a2c2
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-06-09 21:24:51 +0100

    feat: add code to workflow.yml so that workflow reads GitHub Actions Secrets and sets appropriate Terraform variables. Add code to lambda.tf and 3_lambda.tf to give first and third lambda functions access to the Terraform variables that the GitHub Actions workflow set to GitHub Actions Secrets.

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -40,3 +40,112 @@ variable "alert_email_address"{
     sensitive = true
     description = "email for alerts"
 }
+
+
+#****************************************************************
+
+# Variables that will contain 
+# credentials for accessing
+# the ToteSys and warehouse
+# databases, and AWS. 
+# GitHub Actions (GHA)
+# will set these as they are
+# specified in workflow.yml:
+
+# For the ToteSys database
+#*************************
+variable "tf_totesys_db_user" {
+#GHA: TF_TOTESYS_DB_USER: "${{ secrets.TF_TOTESYS_DB_USER }}"    
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds user string for ToteSys db"
+                              }
+
+
+variable "tf_totesys_db_host" {
+# GHA: TF_TOTESYS_DB_HOST: '${{ secrets.TF_TOTESYS_DB_HOST }}'    
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds host string for ToteSys db"
+}
+
+variable "tf_totesys_db_db" {
+# GHA: TF_TOTESYS_DB_DB: '${{ secrets.TF_TOTESYS_DB_DB }}'        
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds database string for ToteSys db"
+}
+
+variable "tf_totesys_db_password" {
+# GHA: TF_TOTESYS_DB_PASSWORD: '${{ secrets.TF_TOTESYS_DB_PASSWORD }}'            
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds password string for ToteSys db"
+}
+
+
+variable "tf_totesys_db_port" {
+# GHA: TF_TOTESYS_DB_PORT: '${{ secrets.TF_TOTESYS_DB_PORT }}'            
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds string for port on which to contact ToteSys db"
+}
+
+# For the warehouse database
+#***************************
+variable "tf_warehouse_db_db" {
+# GHA: TF_WAREHOUSE_DB_DB: '${{ secrets.TF_WAREHOUSE_DB_DB }}'            
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds database string for warehouse db"
+}
+
+
+variable "tf_warehouse_db_host" {
+# GHA: TF_WAREHOUSE_DB_HOST: '${{ secrets.TF_WAREHOUSE_DB_HOST }}'            
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds host string for warehouse db"
+}
+
+
+variable "tf_warehouse_db_password" {
+# GHA: TF_WAREHOUSE_DB_PASSWORD: '${{ secrets.TF_WAREHOUSE_DB_PASSWORD }}'            
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds password string for warehouse db"
+}
+
+
+variable "tf_warehouse_db_port" {
+# GHA: TF_WAREHOUSE_DB_PORT: '${{ secrets.TF_WAREHOUSE_DB_PORT }}'            
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds string for port on which to contact warehouse db"
+}
+
+variable "tf_warehouse_db_user" {
+# GHA: TF_WAREHOUSE_DB_USER: '${{ secrets.TF_WAREHOUSE_DB_USER }}'            
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds string for user of warehouse db"
+}
+
+
+
+# For AWS
+#*****************
+variable "tf_aws_access_key_id" {
+#GHA: TF_AWS_ACCESS_KEY_ID: "${{ secrets.TF_AWS_ACCESS_KEY_ID }}"    
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds string for AWS access key ID"
+}
+
+
+variable "tf_aws_secret_key_id" {
+#GHA:  TF_AWS_SECRET_KEY_ID: '${{ secrets.TF_AWS_SECRET_KEY_ID }}'    
+  type      = string
+  sensitive = true
+  description = "GHA sets this. Holds string for AWS secret access key"
+}
