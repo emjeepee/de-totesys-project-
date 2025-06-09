403eb7cb55c484f189362584c8e3e6e7d9bd1d0f
Duncan <duncan.cornish.1999@gmail.com>
2025-06-09 15:06:35 +0100

    reformat files, delete weird file with no tests

--- src/First_util_for_3rd_lambda.py
+++ src/First_util_for_3rd_lambda.py
@@ -11,71 +11,71 @@ import pyarrow
 # “2025-06-06 15:51:33.123456/sales_order.parquet”, etc
 
 
-
 # ******************************************************************************
 # NOTE: The third lambda employs these two utility functions:
 # 1) get_pandas_dataFrames(), declared here
-# 2) make_SQL_queries_to_warehouse() declared in file 
+# 2) make_SQL_queries_to_warehouse() declared in file
 #   second_util_for_3rd_lambda.py.
 #
-# get_pandas_dataFrames() employs three other utility functions, also declared 
+# get_pandas_dataFrames() employs three other utility functions, also declared
 # in this file. These are:
 # 1) get_latest_timestamp()
 # 2) get_keys_of_latest_tables()
 # 3) get_pq_files()
 # ******************************************************************************
 
-def get_pandas_dataFrames(proc_bucket, ing_bucket, S3_client, current_ts_key):
-    """ This function will:
-            1) read the process S3 bucket for the latest Parquet files
-            2) returns a dictionary containing pandas dataframes 
-                created from those Parquet files
-            3) achieve 1) and 2) above by calling the following 
-                utility functions (all declared bellow):
-                i)   get_latest_timestamp()
-                ii)  get_keys_of_latest_tables() 
-                iii) get_pq_files() (which employs utility function
-                    return_long_table_name()) 
-        Args:
-            proc_bucket: a string for the name of the processed S3 bucket  
-            ing_bucket:  a string for the name of the ingestion S3 bucket  
-            S3_client: the boto3 S3 client
-            current_ts_y: a string for the key of the current timestamp
-                in the ingestion bucket (will be '***timestamp***')
 
-        Returns:
-            A python dictionary that looks like this:
-                {
-                'dim_date': <pandas dataFrame here>, 
-                . . . , 
-                'facts_sales_order': <pandas dataFrame here>
-                }, where each key is the name of a table in the 
-                warehouse database
-    """ 
-    
+def get_pandas_dataFrames(proc_bucket, ing_bucket, S3_client, current_ts_key):
+    """This function will:
+        1) read the process S3 bucket for the latest Parquet files
+        2) returns a dictionary containing pandas dataframes
+            created from those Parquet files
+        3) achieve 1) and 2) above by calling the following
+            utility functions (all declared bellow):
+            i)   get_latest_timestamp()
+            ii)  get_keys_of_latest_tables()
+            iii) get_pq_files() (which employs utility function
+                return_long_table_name())
+    Args:
+        proc_bucket: a string for the name of the processed S3 bucket
+        ing_bucket:  a string for the name of the ingestion S3 bucket
+        S3_client: the boto3 S3 client
+        current_ts_y: a string for the key of the current timestamp
+            in the ingestion bucket (will be '***timestamp***')
+
+    Returns:
+        A python dictionary that looks like this:
+            {
+            'dim_date': <pandas dataFrame here>,
+            . . . ,
+            'facts_sales_order': <pandas dataFrame here>
+            }, where each key is the name of a table in the
+            warehouse database
+    """
+
     # get latest timestamp string
-    # "1900-01-01 00:00:00": 
+    # "1900-01-01 00:00:00":
     latest_ts_hh_mm_ss = get_latest_timestamp(S3_client, ing_bucket, current_ts_key)
 
-    # get a list of the keys for 
-    # the latest tables in the 
+    # get a list of the keys for
+    # the latest tables in the
     # processed bucket:
     list_of_keys = get_keys_of_latest_tables(S3_client, proc_bucket, latest_ts_hh_mm_ss)
 
-    # get the actual objects in 
-    # the processed bucket 
-    # associated with the keys 
+    # get the actual objects in
+    # the processed bucket
+    # associated with the keys
     # in the list just created,
     # convert them into pandas
-    # dataFrames and put them in 
+    # dataFrames and put them in
     # a list:
     dict_of_dataFrames = get_pq_files(S3_client, list_of_keys, proc_bucket)
 
-    # dict_of_dataFrames will look 
+    # dict_of_dataFrames will look
     # like this:
     # {
-    # 'dim_date': dim_table.parquet, 
-    # ... , 
+    # 'dim_date': dim_table.parquet,
+    # ... ,
     # 'facts_sales_order': facts_sales_order.parquet
     # }, where each key is the name of a table
     # in the warehouse database.
@@ -83,17 +83,10 @@ def get_pandas_dataFrames(proc_bucket, ing_bucket, S3_client, current_ts_key):
     return dict_of_dataFrames
 
 
-
-
-
-
-
-
-
 def get_keys_of_latest_tables(S3_client, proc_bucket, ts_prefix):
     """
     This function:
-        looks for all keys in the processed bucket that 
+        looks for all keys in the processed bucket that
             contain string ts_prefix at the beginning
             and returns them
 
@@ -107,21 +100,14 @@ def get_keys_of_latest_tables(S3_client, proc_bucket, ts_prefix):
         a python list of strings, each string being the key
         for the latest files in the processed bucket
 
-            """
-    response = S3_client.list_objects_v2(
-        Bucket=proc_bucket, 
-        Prefix=ts_prefix
-                                        )
-    
+    """
+    response = S3_client.list_objects_v2(Bucket=proc_bucket, Prefix=ts_prefix)
+
     list_of_dicts = response["Contents"]
 
-    return [  dict['Key']   for dict in list_of_dicts  ] # list of strings that are names of the S3 keys
-
-
-
-
-
-
+    return [
+        dict["Key"] for dict in list_of_dicts
+    ]  # list of strings that are names of the S3 keys
 
 
 def get_pq_files(S3_client, key_list, proc_bckt):
@@ -134,56 +120,56 @@ def get_pq_files(S3_client, key_list, proc_bckt):
 
     Args:
         S3_client: a boto3 s3 client
-        key_list: a python list of strings, each string being 
-            a key for an object in the processed bucket 
+        key_list: a python list of strings, each string being
+            a key for an object in the processed bucket
         proc_bckt: a string for the name of the processed bucket
 
     Returns:
-        A list of pandas dataFrames, each dataFrame representing 
+        A list of pandas dataFrames, each dataFrame representing
             a Parquet file that came from the processed bucket,
             each Parquet file corresponding to a key in the list
             of keys passed in.
-    """    
+    """
 
     # fact_sales_order
-# dim_staff
-# dim_location
-# dim_design
-# dim_date
-# dim_currency
-# dim_counterparty
+    # dim_staff
+    # dim_location
+    # dim_design
+    # dim_date
+    # dim_currency
+    # dim_counterparty
 
-    # make a template of the 
+    # make a template of the
     # dictionary to return:
     dict_of_dataFrames = {
-        'fact_sales_order': None,
-        'dim_staff': None,
-        'dim_location': None,
-        'dim_design': None,
-        'dim_date': None,
-        'dim_currency': None,
-        'dim_counterparty': None,
-                         }
+        "fact_sales_order": None,
+        "dim_staff": None,
+        "dim_location": None,
+        "dim_design": None,
+        "dim_date": None,
+        "dim_currency": None,
+        "dim_counterparty": None,
+    }
 
-# loop through the passed-in list of keys:
+    # loop through the passed-in list of keys:
     for table_key in key_list:
         # get the short table name from the key:
         # 'xxxxxx/date.parquet'
-        table_name = table_key.split('/')[-1].split('.')[0] # eg 'date'
-        # knowing the short table name 
-        # get the proper (long) table 
-        # name (for example 
+        table_name = table_key.split("/")[-1].split(".")[0]  # eg 'date'
+        # knowing the short table name
+        # get the proper (long) table
+        # name (for example
         # return_long_table_name()
         # converts 'date' to 'dim_date'):
         long_table_name = return_long_table_name(table_name)
-        # get the object in the processed 
-        # bucket according to its key and 
+        # get the object in the processed
+        # bucket according to its key and
         # convert it to a pandas dataFrame:
         response = S3_client.get_object(Key=table_key, Bucket=proc_bckt)
-        pq_bytes_file = response['Body'].read()
+        pq_bytes_file = response["Body"].read()
         parquet_buffer = BytesIO(pq_bytes_file)
-        dataFrame = pd.read_parquet(parquet_buffer, engine='pyarrow')
-        # store the dataFrame in the 
+        dataFrame = pd.read_parquet(parquet_buffer, engine="pyarrow")
+        # store the dataFrame in the
         # dictionary that this function
         # will return:
         dict_of_dataFrames[long_table_name] = dataFrame
@@ -191,36 +177,32 @@ def get_pq_files(S3_client, key_list, proc_bckt):
     return dict_of_dataFrames
 
 
-
 def return_long_table_name(short_name):
     """
     This function:
         1) is employed by utility function get_pq_files()
         2) is a lookup table
-        3) returns a long table name for the short 
+        3) returns a long table name for the short
             table name passed in
     Args:
         short_name: a string that is the short form
             of the name of a table in the warehouse
             database, eg 'date' for 'dim_date' or
-            'sales_order' for 'facts_sales_order'            
+            'sales_order' for 'facts_sales_order'
     Returns:
-        the long table name version of a short 
-        table name.            
+        the long table name version of a short
+        table name.
     """
     lookup_dict = {
-        'sales_order': 'facts_sales_order',
-        'staff': 'dim_staff',
-        'location': 'dim_location',
-        'design': 'dim_design',
-        'date': 'dim_date',
-        'currency': 'dim_currency',
-        'counterparty': 'dim_counterparty',
-                     }
+        "sales_order": "facts_sales_order",
+        "staff": "dim_staff",
+        "location": "dim_location",
+        "design": "dim_design",
+        "date": "dim_date",
+        "currency": "dim_currency",
+        "counterparty": "dim_counterparty",
+    }
     return lookup_dict[short_name]
-
-
-
 
 
 def get_latest_timestamp(S3_client, bckt, key):
@@ -234,9 +216,8 @@ def get_latest_timestamp(S3_client, bckt, key):
             string (which the ingestion bucket holds)
     Returns:
         A string that is the timestamp
-        
+
 
     """
     response = S3_client.get_object(Bucket=bckt, Key=key)
     return response["Body"].read().decode("utf-8")
-    
--- tests/test_get_keys_of_latest_tables.py
+++ tests/test_get_keys_of_latest_tables.py
@@ -3,19 +3,16 @@ from unittest.mock import Mock
 from src.First_util_for_3rd_lambda import get_keys_of_latest_tables
 
 
-
-
-
 def test_get_keys_of_latest_tables_returns_correct_keys():
     # Arrange:
     mock_s3_client = Mock()
     mock_response = {
         "Contents": [
             {"Key": "2025-06-06 00:00:00/date.parquet"},
-            {"Key": "2025-06-06 00:00:00/sales_order.parquet"}
-                    ]
-                    }
-    
+            {"Key": "2025-06-06 00:00:00/sales_order.parquet"},
+        ]
+    }
+
     mock_s3_client.list_objects_v2.return_value = mock_response
 
     proc_bucket = "test-bucket"
@@ -23,21 +20,15 @@ def test_get_keys_of_latest_tables_returns_correct_keys():
 
     expected_list = [
         "2025-06-06 00:00:00/date.parquet",
-        "2025-06-06 00:00:00/sales_order.parquet"
-                     ]
+        "2025-06-06 00:00:00/sales_order.parquet",
+    ]
 
     # act:
-    result_list = get_keys_of_latest_tables(
-        mock_s3_client, 
-        proc_bucket, 
-        ts_prefix
-                                            )
+    result_list = get_keys_of_latest_tables(mock_s3_client, proc_bucket, ts_prefix)
 
     # assert
     assert result_list == expected_list
-    
-    mock_s3_client.list_objects_v2.assert_called_once_with(
-        Bucket=proc_bucket, 
-        Prefix=ts_prefix
-                                                          )
 
+    mock_s3_client.list_objects_v2.assert_called_once_with(
+        Bucket=proc_bucket, Prefix=ts_prefix
+    )
