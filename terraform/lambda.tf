5d1109516d12954f32f861acc476bf19c8fadacc
Duncan <duncan.cornish.1999@gmail.com>
2025-06-10 11:50:24 +0100

    create new layer for src files

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,7 +5,7 @@
 data "archive_file" "first_lambda_archive" {
   type             = "zip"
   output_file_mode = "0666"
-  source_file       = "${path.module}/../src/lambda_handler.py" # change this to the file with the lambda handler in it
+  source_file      = "${path.module}/../src/lambda_handler.py" # change this to the file with the lambda handler in it
   output_path      = "${path.module}/../function_1.zip" # place to store the zip before uploading to s3
 }
 
@@ -14,6 +14,13 @@ data "archive_file" "layer_archive" {
   output_file_mode = "0666"
   source_dir       = "${path.module}/../layer/" # change this to the layer
   output_path      = "${path.module}/../layer.zip" # where to store the layer before uploading to s3
+}
+
+data "archive_file" "src_layer_archive" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_dir       = "${path.module}/../src/"
+  output_path      = "${path.module}/../src.zip"
 }
 
 resource "aws_s3_object" "first_lambda_deployment" {
@@ -30,6 +37,13 @@ resource "aws_s3_object" "layer_deployment" {
   source_hash = data.archive_file.layer_archive.output_base64sha256
 }
 
+resource "aws_s3_object" "src_layer_deployment" {
+  bucket = aws_s3_bucket.lambda-bucket.bucket
+  key    = "layer/src_layer.zip"
+  source = data.archive_file.src_layer_archive.output_path
+  source_hash = data.archive_file.src_layer_archive.output_base64sha256
+}
+
 resource "aws_lambda_layer_version" "layer" {
   layer_name          = "layer"
   compatible_runtimes = [var.python_runtime]
@@ -37,11 +51,18 @@ resource "aws_lambda_layer_version" "layer" {
   s3_key              = aws_s3_object.layer_deployment.key
 }
 
+resource "aws_lambda_layer_version" "src_layer" {
+  layer_name          = "src_layer"
+  compatible_runtimes = [var.python_runtime]
+  s3_bucket           = aws_s3_bucket.lambda-bucket.bucket
+  s3_key              = aws_s3_object.src_layer_deployment.key
+}
+
 resource "aws_lambda_function" "extract_handler" {
   s3_bucket        = aws_s3_bucket.lambda-bucket.bucket
   s3_key           = aws_s3_object.first_lambda_deployment.key
 
-  layers           = [aws_lambda_layer_version.layer.arn]
+  layers           = [aws_lambda_layer_version.layer.arn, aws_lambda_layer_version.src_layer.arn]
 
   function_name    = var.first_lambda_function
   role             = aws_iam_role.first_lambda_function_role.arn
