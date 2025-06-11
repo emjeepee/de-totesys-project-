afd98b4598598331accb91e1d7fbc3bb728480d8
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-06-11 09:19:18 +0100

    fix: some more final corrections, this time to test_conn_to_db.py and conn_to_db.py

--- tests/test_conn_to_db.py
+++ tests/test_conn_to_db.py
@@ -8,11 +8,16 @@ from src.conn_to_db import conn_to_db
 def test_conn_to_db_connects_to_the_database():
     with patch("src.conn_to_db.Connection") as mock_Connection_to_db:
         DB_NAME = "TESTDB"
-        os.environ["TOTESYS_DB_USER"] = "test_db_name"
-        os.environ["TOTESYS_DB_PASSWORD"] = "test_db_name"
-        os.environ["TOTESYS_DB_DB"] = "test_db_name"
-        os.environ["TOTESYS_DB_HOST"] = "test_db_name"
-        os.environ["TOTESYS_DB_PORT"] = "12345"
+        os.environ[f"TF_{DB_NAME}_DB_USER"] = "test_db_name"
+        os.environ[f"TF_{DB_NAME}_DB_PASSWORD"] = "test_db_name"
+        os.environ[f"TF_{DB_NAME}_DB_DB"] = "test_db_name"
+        os.environ[f"TF_{DB_NAME}_DB_HOST"] = "test_db_name"
+        os.environ[f"TF_{DB_NAME}_DB_PORT"] = "test_db_name"
+
+        # os.environ["TOTESYS_DB_PASSWORD"] = "test_db_name"
+        # os.environ["TOTESYS_DB_DB"] = "test_db_name"
+        # os.environ["TOTESYS_DB_HOST"] = "test_db_name"
+        # os.environ["TOTESYS_DB_PORT"] = "12345"
 
         return_value = conn_to_db()
         assert return_value == mock_Connection_to_db(ANY, ANY, ANY, ANY, ANY, ANY)
