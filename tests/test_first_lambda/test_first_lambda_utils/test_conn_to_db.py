d4d92cbfe7d814b5e0f59f46b28b83002716d3db
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-07-30 22:37:29 +0100

    Mukund solo work to change file test_conn_to_db.py so that its reference to the pg8000.native Connection object in the patch code is correct (as the path to file conn_to_db had changed).

---
+++ tests/test_first_lambda/test_first_lambda_utils/test_conn_to_db.py
@@ -0,0 +1,20 @@
+from src.first_lambda.first_lambda_utils.conn_to_db import conn_to_db
+from unittest.mock import Mock, patch, ANY
+import os
+
+
+
+def test_conn_to_db_connects_to_the_database():
+    with patch("src.first_lambda.first_lambda_utils.conn_to_db.Connection") as mock_Connection_to_db:
+        DB_NAME = "TESTDB"
+        os.environ[f"TF_{DB_NAME}_DB_USER"] = "test_db_name"
+        os.environ[f"TF_{DB_NAME}_DB_PASSWORD"] = "test_db_pword"
+        os.environ[f"TF_{DB_NAME}_DB_DB"] = "test_db_db_name"
+        os.environ[f"TF_{DB_NAME}_DB_HOST"] = "test_db_host"
+        os.environ[f"TF_{DB_NAME}_DB_PORT"] = "test_db_port"
+
+        return_value = conn_to_db()
+        assert return_value == mock_Connection_to_db(ANY, ANY, ANY, ANY, ANY, ANY)
+
+
+
