164110a8d46f3e435d4816e80732559d35f9b8b5
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-08-01 18:00:32 +0100

    Mukund solo work. made changes to test file and code file for function change_after_time_timestamp. The changes involved updating file paths as I had changed the project directory structure

--- src/first_lambda/first_lambda_utils/change_after_time_timestamp.py
+++ src/first_lambda/first_lambda_utils/change_after_time_timestamp.py
@@ -1,8 +1,5 @@
-from first_lambda_utils.write_to_ingestion_bucket import create_formatted_timestamp
-
-import botocore
 from botocore.exceptions import ClientError
-from datetime import datetime, timedelta, UTC
+from datetime import datetime, UTC
 
 
 def change_after_time_timestamp(bucket_name, s3_client, ts_key, default_ts):
@@ -10,38 +7,39 @@ def change_after_time_timestamp(bucket_name, s3_client, ts_key, default_ts):
     This function:
         1) is the first utility function that 
             the first lambda function calls.
-        2) reads the timestamp string in the 
-            S3 ingestion bucket and returns it.
+        2) tries to read the timestamp string 
+            that the S3 ingestion bucket stores. 
+            On success it returns the timestamp.
+            On failure this function returns
+            the default timestamp (for the year
+            1900).
         3) overwrites the existing timestamp 
-            string in the S3 bucket with a 
-            a timestamp string that represents
-            the current time.
-        4) returns the existing timestamp string
-            if reading the S3 ingestion bucket
-            is successful.
-        5) returns the default timestamp string
-            if reading the S3 ingestion bucket
-            is not successful.
+            string in the bucket with one it
+            creates for the current time.
             
     Args:
         1) bucket_name: a string for the name of the
             S3 ingestion bucket.
         2) s3_client: the boto3 S3 client.
-        3) ts_key: the key under which the timstamp
+        3) ts_key: the key under which the timestamp
             is saved in the bucket (it is actually
             always "***timestamp***").
-        4) default_ts: a timestamp this function 
-            returns on its very first read of the
-            ingestion bucket (ie "1900-01-01-00-00-00")
+        4) default_ts: set to "1900-01-01T00-00-00".
+            This is the timestamp this function 
+            returns either
+             i)  on its first ever read of the
+                 ingestion bucket or
+             ii) if the attempt to get the timestamp
+                 from the bucket fails.
 
 
     Returns:
         Either --
-            1) the saved timestamp if reading the 
+            1) the previous timestamp if reading the 
                 bucket is a success. The first time ever
                 that the lambda function runs this 
-                timestamp will be "1900-01-01-00-00-00".
-            2) timestamp "1900-01-01-00-00-00" if  
+                timestamp will be "1900-01-01T00-00-00".
+            2) timestamp "1900-01-01T00-00-00" if  
                 reading the bucket fails.
 
     """
