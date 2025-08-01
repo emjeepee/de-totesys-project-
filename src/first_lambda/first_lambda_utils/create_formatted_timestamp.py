e549570146c35a9b6c59e6ed765a125b95bcdc7a
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-08-01 14:56:11 +0100

    Mukund solo work successfully tested function create_formatted_timestamp()

--- src/first_lambda/first_lambda_utils/create_formatted_timestamp.py
+++ src/first_lambda/first_lambda_utils/create_formatted_timestamp.py
@@ -4,11 +4,15 @@ def create_formatted_timestamp():
     """
     This function:
         1) creates a timestamp string formatted like this:
-            '2025-06-13_13-13-13_170000'
+            '2025-06-13_13-13-13'
 
     Returns:
         The formatted timestamp string
     """
+
+    # Make a datetime object that includes 
+    # the current time and time zone info:
     now_dt_object = datetime.now(timezone.utc)
+
     formatted_ts = now_dt_object.strftime("%Y-%m-%d_%H-%M-%S")
     return formatted_ts
