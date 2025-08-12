26f119d6a69cd89dfa3b25d4c0cf6ec8853e7663
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-08-12 12:19:41 +0100

    Mukund solo work. Minor changes to second and third lambda functions

--- src/third_lambda/third_lambda_utils/make_SQL_qry_for_one_row.py
+++ src/third_lambda/third_lambda_utils/make_SQL_qry_for_one_row.py
@@ -29,3 +29,44 @@ def make_SQL_qry_for_one_row(table_name: str, pk_col, cols, vals, row):
                 f"INSERT INTO {table_name} ({cols}) VALUES ({vals});"
                             )   
     return sql_query_str        
+
+
+# IMPORTANT!!!:
+
+
+
+# from datetime import date
+
+# # Get today's date
+# created_date = date.today()
+
+# # Convert to string for SQL
+# created_date_str = created_date.strftime("%Y-%m-%d")
+
+# print(created_date_str)  # e.g., '2025-08-11'
+
+
+# NOTE IMPORTANT!!!: 
+# 1) dim_date 
+# has a date_id column whose value must be an SQL date
+# (the rest are ints or varchars)
+# 2) dim_staff 
+# has a column  whose value must be an SQL email address
+# (the rest are ints or varchars)
+# 3) fact_sales_order 
+# has a created_date column whose values must be SQL dates
+# has a created_time column whose values must be SQL times
+# has a last_updated_date column whose values must be SQL dates
+# has a last_updated_time column whose values must be SQL times
+# has a unit_price column whose values must be SQL numeric(10,2)
+# has a agreement_payment_date column whose values must be SQL dates
+# has a agreement_delivery_date column whose values must be SQL dates
+# (the rest are ints or varchars)
+
+
+# These tables have columns whose values are 
+# all either varchars or ints:
+# dim_counterparty
+# dim_currency
+# dim_design
+# dim_location
