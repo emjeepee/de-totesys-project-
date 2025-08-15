de5ac0974b67dcd72e836f76b42094fba10ce040
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-08-15 09:47:16 +0100

    Mukund solo work. Forgot to make a separate branch and did some minor things in the main branch

---
+++ tests/test_first_lambda/test_first_lambda_utils/test_first_lambda_init.py
@@ -0,0 +1,53 @@
+from src.first_lambda.first_lambda_utils.first_lambda_init import first_lambda_init
+
+import pytest
+
+
+
+
+# @pytest.mark.skip
+def test_returns_a_dictionary():
+    # Arrange
+    expected = dict
+
+
+    # Act
+    returned = first_lambda_init()
+    result = type(returned)
+
+    # Assert
+
+    assert True
+
+
+
+
+
+
+@pytest.mark.skip
+def test_xxxxx():
+    # Arrange
+
+    # Act
+
+
+    # Assert
+
+    assert True
+
+
+
+
+
+
+
+@pytest.mark.skip
+def test_xxxxx():
+    # Arrange
+
+    # Act
+
+
+    # Assert
+
+    assert True
