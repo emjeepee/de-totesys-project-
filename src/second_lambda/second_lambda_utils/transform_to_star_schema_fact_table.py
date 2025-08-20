4b55ece2fe45c480297ecc9933ff4a7b80a11235
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-08-20 20:14:28 +0100

    Mukund solo work. Successful unit testing of second lambda utility function transform_to_star_schema_fact_table

--- src/second_lambda/second_lambda_utils/transform_to_star_schema_fact_table.py
+++ src/second_lambda/second_lambda_utils/transform_to_star_schema_fact_table.py
@@ -1,3 +1,5 @@
+from datetime import datetime, date, time
+
 
 
 def transform_to_star_schema_fact_table(table_data):
@@ -33,10 +35,21 @@ def transform_to_star_schema_fact_table(table_data):
     # last_updated_time       time            14:35:20  
     # agreed_payment_date     date            2024-01-01  
     # agreed_delivery_date    date            2024-01-01  
+    # unit_price            numeric(10,2)       2.45  
     #    All the rest are ints
 
-    fact_sales_order = []
 
+    # the sales_orders table as retrieved from
+    # the ingestion bucket looks like this:
+            #             [
+            #  {'sales_order_id': 15647, 'created_at': datetime(2025, 8, 13, 9, 47, 9, 901000), 'last_updated': datetime(2025, 8, 13, 9, 47, 9, 901000),  'design_id': 648,  'staff_id': 19,  'counterparty_id': 14, 'units_sold': 36692, 'unit_price': Decimal('2.40'), 'currency_id': 2, 'agreed_delivery_date': '2025-08-20', 'agreed_payment_date': '2025-08-16', 'agreed_delivery_location_id': 11},
+            #  {'sales_order_id': 15648, 'created_at': datetime(2025, 9, 13, 9, 47, 9, 901000), 'last_updated': datetime(2025, 9, 13, 9, 47, 9, 901000),  'design_id': 649,  'staff_id': 19,  'counterparty_id': 12, 'units_sold': 36692, 'unit_price': Decimal('2.40'), 'currency_id': 2, 'agreed_delivery_date': '2025-09-20', 'agreed_payment_date': '2025-09-16', 'agreed_delivery_location_id': 12},
+            #    etc
+            #             ]
+
+
+
+    fact_sales_order = []
 
     for row in table_data:
         # For each row of the table
@@ -44,7 +57,7 @@ def transform_to_star_schema_fact_table(table_data):
         # (a datetime object) and 
         # convert to datetime date 
         # and datetime time objects. 
-        # Do the same for other  
+        # Do similar for other  
         # values:
         dt_created = row.get("created_at")
         dt_created_time = dt_created.time() # extract time only
@@ -54,29 +67,33 @@ def transform_to_star_schema_fact_table(table_data):
         dt_updated_time = dt_updated.time() # extract time only
         dt_updated_date = dt_updated.date() # extract date only
 
-        dt_apd = row.get("agreed_payment_date")
-        dt_apd = dt_apd.date()  # extract date only
+        apd_str = row.get("agreed_payment_date") # eg '2025-08-16'
+        dt_apd = datetime.strptime(apd_str, "%Y-%m-%d").date()
 
-        dt_add = row.get("agreed_delivery_date")
-        dt_add = dt_add.date()  # extract date only
+        add_str = row.get("agreed_delivery_date") # eg '2025-09-20'
+        dt_add = datetime.strptime(add_str, "%Y-%m-%d").date()
 
-
-        table_row = {
-                "sales_order_id": row.get("sales_order_id"),
-                "created_date": dt_created_date,
-                "created_time": dt_created_time,
-                "last_updated_date": dt_updated_date,
-                "last_updated_time": dt_updated_time,
-                "sales_staff_id": row.get("staff_id"),
-                "counterparty_id": row.get("counterparty_id"),
-                "units_sold": row.get("units_sold"),
-                "unit_price": row.get("unit_price"),
-                "currency_id": row.get("currency_id"),
-                "design_id": row.get("design_id"),
-                "agreed_payment_date": dt_apd,
-                "agreed_delivery_date": dt_add,
-                "agreed_delivery_location_id": row.get("agreed_delivery_location_id"),
-                        }
+        up_dec = row.get("unit_price")
+        up_form = f"{up_dec:.2f}" # eg '3.56'
         
-    fact_sales_order.append(table_row)
+
+        facts_table_row = {
+                "sales_order_id": row.get("sales_order_id"),    # type is int
+                "created_date": dt_created_date,                # type is datetime.date 
+                "created_time": dt_created_time,                # type is datetime.time 
+                "last_updated_date": dt_updated_date,           # type is datetime.date 
+                "last_updated_time": dt_updated_time,           # type is datetime.time 
+                "sales_staff_id": row.get("staff_id"),          # type is int
+                "counterparty_id": row.get("counterparty_id"),  # type is int
+                "units_sold": row.get("units_sold"),            # type is int
+                "unit_price": up_form,                          # type is string
+                "currency_id": row.get("currency_id"),          # type is int
+                "design_id": row.get("design_id"),              # type is int
+                "agreed_payment_date": dt_apd,                  # type is datetime.date 
+                "agreed_delivery_date": dt_add,                 # type is datetime.date 
+                "agreed_delivery_location_id": \
+                    row.get("agreed_delivery_location_id"),     # type is int
+                        }
+       
+        fact_sales_order.append(facts_table_row)
     return fact_sales_order
--- tests/test_second_lambda/test_second_lambda_utils/test_transform_to_star_schema_fact_table.py
+++ tests/test_second_lambda/test_second_lambda_utils/test_transform_to_star_schema_fact_table.py
@@ -1,6 +1,6 @@
 import pytest
 
-from datetime import datetime
+from datetime import datetime, date, time
 from decimal import Decimal
 
 from src.second_lambda.second_lambda_utils.transform_to_star_schema_fact_table import transform_to_star_schema_fact_table
@@ -8,38 +8,107 @@ from src.second_lambda.second_lambda_utils.transform_to_star_schema_fact_table i
 
 @pytest.fixture
 def general_setup():
+        keys = [
+                "sales_order_id",
+                "created_date",
+                "created_time",
+                "last_updated_date",
+                "last_updated_time",
+                "sales_staff_id",
+                "counterparty_id",
+                "units_sold",
+                "unit_price",
+                "currency_id",
+                "design_id",
+                "agreed_payment_date",
+                "agreed_delivery_date",
+                "agreed_delivery_location_id"
+                       ]
+        
+        types =     [
+             int,
+             date,
+             time,
+             date,
+             time,
+             int,
+             int,
+             int,
+             str,
+             int,
+             int,
+             date,
+             date,
+             int
+                    ]
 
 
         mock_so_table = [
              {'sales_order_id': 15647, 'created_at': datetime(2025, 8, 13, 9, 47, 9, 901000), 'last_updated': datetime(2025, 8, 13, 9, 47, 9, 901000),  'design_id': 648,  'staff_id': 19,  'counterparty_id': 14, 'units_sold': 36692, 'unit_price': Decimal('2.40'), 'currency_id': 2, 'agreed_delivery_date': '2025-08-20', 'agreed_payment_date': '2025-08-16', 'agreed_delivery_location_id': 11},
-             {'sales_order_id': 15648, 'created_at': datetime(2025, 9, 13, 9, 47, 9, 901000), 'last_updated': datetime(2025, 9, 13, 9, 47, 9, 901000),  'design_id': 649,  'staff_id': 19,  'counterparty_id': 12, 'units_sold': 36692, 'unit_price': Decimal('2.40'), 'currency_id': 2, 'agreed_delivery_date': '2025-09-20', 'agreed_payment_date': '2025-09-16', 'agreed_delivery_location_id': 12},
+             {'sales_order_id': 15648, 'created_at': datetime(2025, 9, 13, 9, 47, 9, 901000), 'last_updated': datetime(2025, 9, 13, 9, 47, 9, 901000),  'design_id': 649,  'staff_id': 19,  'counterparty_id': 12, 'units_sold': 36692, 'unit_price': Decimal('4.63'), 'currency_id': 1, 'agreed_delivery_date': '2025-09-20', 'agreed_payment_date': '2025-09-16', 'agreed_delivery_location_id': 12},
                         ]
 
+        yield keys, types, mock_so_table
 
 
-        yield mock_so_table
+
 
 
 # @pytest.mark.skip
 def test_returns_list(general_setup):
     # Arrange
-    () = general_setup
+    ( keys, types, mock_so_table) = general_setup
 
     expected = list
 
     # Act
-    # transform_to_star_schema_fact_table(table_data)
+    response = transform_to_star_schema_fact_table(mock_so_table)
+    result = type(response)
     # result = None
     
 
     # Assert
-    assert result = expected
+    assert result == expected
 
 
 
 
+# @pytest.mark.skip
+def test_fact_table_has_same_number_or_rows_as_sales_order_table(general_setup):
+    # Arrange:
+    ( keys, types, mock_so_table) = general_setup
+    expected = len(mock_so_table)        
+
+    # Act:
+    response = transform_to_star_schema_fact_table(mock_so_table) # [{}, {}, {}, etc]
+    result = len(response)
+    # result = None
+    
+    # Assert:
+    assert result == expected
 
 
+
+
+# @pytest.mark.skip
+def test_each_row_of_fact_table_has_correct_keys(general_setup):
+    # Arrange:
+    ( keys, types, mock_so_table) = general_setup
+    is_equal = True
+
+    # Act:
+    response = transform_to_star_schema_fact_table(mock_so_table) # [{}, {}, {}, etc]
+    for dict in response:
+        for i in range(14):
+             if not list(dict.keys())[i] == keys[i]: 
+                is_equal = not is_equal
+                break
+
+    result = is_equal
+    # result = False
+    
+    # Assert:
+    assert result 
 
 
 
@@ -48,39 +117,28 @@ def test_returns_list(general_setup):
 
 
 # @pytest.mark.skip
-def test_xxxx(general_setup):
+def test_each_row_of_fact_table_has_values_of_correct_type(general_setup):
     # Arrange
-    () = general_setup
-
-    
+    ( keys, types, mock_so_table) = general_setup
+    is_equal = True
+   
 
     # Act
-    
-    
-    # result = None
-    
+    response = transform_to_star_schema_fact_table(mock_so_table) # [{}, {}, {}, etc]
+    for dict in response:
+        for i in range(14):
+             val = list(dict.values())[i]
+             val_type = type(val)
+             if not val_type == types[i]: 
+                is_equal = not is_equal
+                break
+
+    result = is_equal
+    # result = False
+
 
     # Assert
-    assert True
-
-
-
-
-# @pytest.mark.skip
-def test_xxxx(general_setup):
-    # Arrange
-    () = general_setup
-
-    
-
-    # Act
-    
-    
-    # result = None
-    
-
-    # Assert
-    assert True
+    assert result
 
 
 
