0def236b1238cdc1134a8227368df98b63c13e63
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-08-20 13:41:21 +0100

    Mukund solo work. minor changes

---
+++ tests/test_second_lambda/test_second_lambda_utils/test_make_dim_or_fact_table.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+# @pytest.mark.skip
+def test_xxx(general_setup):
+    # Arrange:
+    () = general_setup
+
+    # Act:
+    # make_dim_or_fact_table(table_name: str, table_python, s3_client, ingestion_bucket: str)
+
+
+    # Assert:
+    assert True
+
+
+
+# @pytest.mark.skip
+def test_xxx(general_setup):
+    # Arrange:
+    () = general_setup
+
+    # Act:
+    # make_dim_or_fact_table(table_name: str, table_python, s3_client, ingestion_bucket: str)
+
+
+    # Assert:
+    assert True
