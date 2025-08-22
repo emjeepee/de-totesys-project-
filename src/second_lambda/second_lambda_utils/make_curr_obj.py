5baf0e28eed3bcdafc3504197653f0e312d690cf
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-08-22 09:05:28 +0100

    Mukund solo work. For got to create separate branch. Started unit testing of second lambda utility functions transform_to_dim_currency and make_curr_obj

--- src/second_lambda/second_lambda_utils/make_curr_obj.py
+++ src/second_lambda/second_lambda_utils/make_curr_obj.py
@@ -1,22 +1,45 @@
-from currency_codes import get_currency_by_code, Currency
+from currency_codes import get_currency_by_code, Currency, CurrencyNotFoundError
 
 
 
 def make_curr_obj(currency_dict):
         """
         This function:
-            creates a Currency object that will
-             contain a currency name.
+            1) creates a Currency object that 
+                contains a currency name and 
+                returns that object. 
+            2) creates the currency object by 
+                getting the value of the 
+                'currency_code' key in the 
+                passed-in dictionary, passing
+                that value to the imported
+                get_currency_by_code() and 
+                returning the return value
+                of that function (a 
+                Currency object).
+            3) Gets called by second lambda 
+               utility function 
+               transform_to_dim_currency().   
         
         Args:
             currency_dict: a dictionary that has key 
-            "currency_code", whose value is a code 
-            for a currency.
+             "currency_code", whose value is a code 
+             for a currency. The dictionary 
+             represents a row from the 
+             preprocessed currency dimension table.
 
         Returns:
             a Currency object that contains (among
             other information) the name of a currency.            
 
         """
-        curr_obj = get_currency_by_code(currency_dict.get("currency_code"))
-        return curr_obj
+
+        err_Msg = 'Invalid currency code provided'
+
+        code = currency_dict['currency_code']
+
+        try:
+            curr_obj = get_currency_by_code(code)
+
+        except CurrencyNotFoundError as e:
+            raise RuntimeError(err_Msg) from e
--- src/second_lambda/second_lambda_utils/transform_to_dim_currency.py
+++ src/second_lambda/second_lambda_utils/transform_to_dim_currency.py
@@ -1,3 +1,5 @@
+from currency_codes import get_currency_by_code, Currency, CurrencyNotFoundError
+
 from src.second_lambda.second_lambda_utils.preprocess_dim_tables import preprocess_dim_tables
 from src.second_lambda.second_lambda_utils.make_curr_obj import make_curr_obj
 
@@ -22,6 +24,29 @@ def transform_to_dim_currency(currency_data):
         table.    
     """
 
+
+    pp_curr_dim_table = preprocess_dim_tables(currency_data, ['created_at', 'last_updated'])
+    
+    for row_dict in pp_curr_dim_table: # {'currency_id': 3, 'currency_code': 'EUR'}
+        curr_obj = make_curr_obj(row_dict)
+        curr_name = curr_obj.name
+        row_dict['currency_name'] = curr_name
+
+    # preproc_currency_dim_table is 
+    # now the finished currency 
+    # dimension table. Return it:
+    return pp_curr_dim_table
+
+
+
+# Typical currency table cols and typical values
+# [['currency_id'], ['currency_code'], ['created_at'], ['last_updated']]
+# [[3, 'EUR', datetime.datetime(2022, 11, 3, 14, 20, 49, 962000), datetime.datetime(2022, 11, 3, 14, 20, 49, 962000)]]
+
+
+
+
+
 # OLD CODE:
     # dim_currency = []
 
@@ -41,14 +66,4 @@ def transform_to_dim_currency(currency_data):
     #         }
     #     dim_currency.append(transformed_row)
 
-    # return dim_currency
-
-
-    pp_curr_dim_table = preprocess_dim_tables(currency_data, ['created_at', 'last_updated'])
-    for row_dict in pp_curr_dim_table:
-        row_dict['currency_name'] = make_curr_obj(row_dict).name
-
-    # preproc_currency_dim_table is 
-    # now the finished currency 
-    # dimension table. Return it:
-    return pp_curr_dim_table
+    # return dim_currencys
