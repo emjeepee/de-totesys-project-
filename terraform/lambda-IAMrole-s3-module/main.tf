13414796380999f6522704e33b810846e4cd8c8b
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-08-30 22:15:51 +0100

    Mukund solo work. Making progress sswapping old tf files for versions that employ a tf module.

--- terraform/lambda-IAMrole-s3-module/main.tf
+++ terraform/lambda-IAMrole-s3-module/main.tf
@@ -1,9 +1,73 @@
+# What needs to be provisioned:
+# 1) A lambda function
+# 2) An execution role for the lambda function
+# 3) a standalone policy to let a lambda function
+#    write to a bucket  
+# 4) a policy attachment for the policy in 3)
+# 5) a standalone policy to let a lambda function
+#    read from a bucket  
+# 6) a policy attachment for the policy in 5)
+# 7) an s3 bucket with inline policy that 
+#    has two statements:
+#    i)   one that allows a lambda to write 
+#         data to it  
+#    ii)  one that allows a lambda to read 
+#         data from it  
+# 8) an s3 code bucket with inline policy that 
+#    ???:
+#    ???
 
-# IAM execution role for 
-# the lambda function:
+# The first call of the module will provision:
+# 1) the first lambda
+# 2) the first lambda exec role
+# 3) the policy to let a lambda write to a bucket 
+# 4) the attachment for the policy
+# 5) an s3 bucket (ingestion) with inline policy 
+#    statements that allow the first lambda to 
+#    write to it and the second lambda to read 
+#    from it
+# 6) The code bucket
+
+
+# The 2nd call of the module will provision:
+# 1) the 2nd lambda
+# 2) the 2nd lambda exec role
+# 3) the policy to let a lambda read from a bucket 
+# 4) the attachment for 3)
+# 5) the policy to let a lambda write to a bucket 
+#    (the processed bucket)
+# 6) the attachment for 5)
+# 7) an s3 bucket (processed) with inline policy 
+#    statements that allow the 2nd lambda to write 
+#    to it and the 3rd lambda to read from it
+
+
+# The 3rd call of the module will provision:
+# 1) the 3rd lambda
+# 2) the 3rd lambda exec role
+# 3) the policy to let a lambda read from 
+#    the processed bucket 
+# 4) the attachment for 3)
+
+
+
+
+
+
+
+# Provision an IAM execution role for 
+# a lambda function. (NOTE: "lambda_exec"
+# cannot be dynamic, ie you cannot include 
+# a variable value in that string!! 
+# Instead, if you need several roles just 
+# like this one, use count of for each):
 resource "aws_iam_role" "lambda_exec" {
   name = "${var.lambda_name}-IAM-role"
 
+# Define the trust policy to allow 
+# lambda functions to assume this 
+# role (could also be standalone 
+# instead of inline):
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -17,22 +81,60 @@ resource "aws_iam_role" "lambda_exec" {
 
 
 
-# Provision the lambda function
-# itself:
+# Provision a lambda function:
 resource "aws_lambda_function" "mod_lambda" {
   function_name = var.lambda_name
   role          = aws_iam_role.lambda_exec.arn
   runtime       = var.runtime
   handler       = var.handler
-  s3_bucket     = var.s3_bucket
-  s3_key        = var.s3_function_key
+  s3_bucket     = var.bucket_name # will be code bucket 
+  s3_key        = var.s3_key_for_func
 
   environment {
 #     variables = var.environment_vars
               }
                                             }
 
-# is this needed?:
-output "lambda_arn" {
-  value = aws_lambda_function.mod_lambda.arn
-                    }
+
+# 
+# should_make_s3_get_obj_policy
+
+
+# Provision policy for 
+# the execution role to 
+# allow a lambda to write 
+# to a bucket:
+resource "aws_iam_policy" "lambda_policy" {
+  count = var.should_make_s3_put_obj_policy ? 1 : 0
+
+  name   = "${var.lambda_name}-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["s3:PutObject"],
+      Resource = "arn:aws:s3:::${var.bucket_name}/*"
+    }]
+  })
+}
+
+
+
+# Provision policy attachment
+# for a lambda function
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  count      = var.should_make_s3_put_obj_policy_attach ? 1 : 0
+  role       = aws_iam_role.lambda_exec[0].name
+  policy_arn = aws_iam_policy.lambda_policy[0].arn
+                                                          }
+
+
+
+
+
+
+# Provision an s3 bucket:
+resource "aws_s3_bucket" "mod-buck" {
+  count  = var.should_make_bucket ? 1 : 0
+  bucket = var.bucket_name 
+                                    }
--- terraform/lambda-IAMrole-s3-module/variables.tf
+++ terraform/lambda-IAMrole-s3-module/variables.tf
@@ -3,37 +3,99 @@ variable "lambda_name" {
       description = "name of a lambda function in AWS"
       type        = string
                        }
-variable "s3_bucket_name"  {
+
+variable "bucket_name"  {
       description = "name of ingestion/processed/code bucket in AWS"
       type        = string
-                       }
-variable "s3_function_key"  {
+                          }
+
+variable "s3_key_for_func"  {
       description = "key under which to store a zipped lambda function in the code s3 bucket"
       type        = string
                        }
+
 variable "runtime"  {
       description = "version of Python"
       type        = string
                        }
+
 variable "handler"  {
-      description = "name of the Python function that is the actual handler for the lambda function"
+      description = "name of the Python function that is the lambda handler"
       type        = string
                    }
 
-# Next two are for conditionally 
-# provisioning 
-# 
+
+
+# Var for conditionally 
+# provisioning the ingestion 
+# bucket or the processed 
+# bucket: 
 variable "should_make_bucket" {
   type    = bool
-  default = true
-  description = "when set to true, code will create the bucket"
+  default = false
+  description = "when true, code will create bucket"
                               }
 
-variable "should_make_role" {
+
+# Var for conditionally 
+# provisioning the s3 code bucket: 
+variable "should_make_s3_code_bucket" {
   type    = bool
-  default = true
-  description = "when set to true, code will create the role"
+  default = false
+  description = "when true, code will create the s3 code bucket"
+                                      }
+
+
+
+# Var for conditionally 
+# provisioning an identity-based
+# policy to attach to a lambda's 
+# execution role to allow the 
+# lambda to put an object in an 
+# s3 bucket: 
+variable "should_make_s3_put_obj_policy" {
+  type    = bool
+  default = false
+  description = "when true, code will create a policy to allow a lambda function to put an object in an s3 bucket"
+                                         }
+
+
+# Var for conditionally 
+# provisioning an attachment to 
+# to attach the put object policy 
+# to a lambda's execution role: 
+variable "should_make_s3_put_obj_policy_attach" {
+  type    = bool
+  default = false
+  description = "when true, code will create a policy attachment to allow a policy that allows a lambda function to write to a bucket be attached to the lambda's execution role"
                             }
+
+
+# Var for conditionally 
+# provisioning an identity-based
+# policy to attach to a lambda's 
+# execution role to allow the 
+# lambda to get an object from 
+# an s3 bucket: 
+variable "should_make_s3_get_obj_policy" {
+  type    = bool
+  default = false
+  description = "when true, code will create a policy to allow a lambda function to get an object from an s3 bucket"
+                            }
+
+
+
+# Var for conditionally 
+# provisioning an attachment to 
+# to attach the get object policy 
+# to a lambda's execution role: 
+variable "should_make_s3_get_obj_policy_attach" {
+  type    = bool
+  default = false
+  description = "when true, code will create a policy attachment to allow a policy that allows a lambda function to read from a bucket be attached to the lambda's execution role"
+                                                }
+
+
 
 
 
