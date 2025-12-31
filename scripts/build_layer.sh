c5865ce763e3059764bf6cbd9e1b3a16ff9c33b2
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-12-31 12:08:11 +0000

    Forgot to create a separate branch. Take pip-compile command out of bash script that GitHub Actions runs as this slows GitHub Actions down a lot.

--- scripts/build_layer.sh
+++ scripts/build_layer.sh
@@ -2,20 +2,17 @@
 
 set -euo pipefail
 
-echo "Upgrading pip and installing pip-tools..."
+echo "Upgrading pip, installing pip-tools..."
 python -m pip install --upgrade pip
 python -m pip install pip-tools
 
-echo "Compiling layer requirements..."
-cd requirements
-pip-compile GHA_layer_requirements.in
-cd ..
 
 echo "Installing layer dependencies..."
 mkdir -p layer/python/lib/python3.12/site-packages
 python -m pip install \
   -r requirements/GHA_layer_requirements.txt \
   -t layer/python/lib/python3.12/site-packages
+
 
 echo "Zipping the layer..."
 cd layer
